73eeb5770b88e80eedc3863498b3a5f5f6e678e8
Paul Ganster <paul.ganster@dccs.eu>
2023-10-24 08:57:17 +0200

    Add image gallery
    Running the project now will result into the error "Cannot read properties of null (reading 'useRef')" when opening the host

--- remote/src/components/Button.jsx
+++ remote/src/components/Button.jsx
@@ -2,11 +2,18 @@
 import "./Button.css"
 
 import { useState } from "react"
+import ImageGallery from 'react-image-gallery';
 
 export const Button = () => {
-  const [state, setState] = useState(0)
+  const [state, setState] = useState(0);
+  const items = [{
+      original: 'https://upload.wikimedia.org/wikipedia/commons/7/74/White_domesticated_duck,_stretching.jpg',
+      thumbnail: 'https://upload.wikimedia.org/wikipedia/commons/7/74/White_domesticated_duck,_stretching.jpg'
+  }];
   return (
     <div>
+      <ImageGallery items={items} />
+
       <button id='click-btn' className='shared-btn' onClick={() => setState((s) => s + 1)}>Click me: {state}</button>
     </div>
   )
